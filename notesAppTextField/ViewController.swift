fecd3b6f7992092b90741533a750d1246d8b1b7f
James Durcan <jdurcan05@gmail.com>
2022-09-01 07:44:05 -0500

    Finished Notes app

--- notesAppTextField/ViewController.swift
+++ notesAppTextField/ViewController.swift
@@ -7,13 +7,30 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITextFieldDelegate {
+    @IBOutlet weak var textFieldOutlet: UITextField!
+    @IBOutlet weak var labelOutlet: UILabel!
+    @IBOutlet weak var ageOutlet: UITextField!
+    var theName: String!
+    var theAge: String!
     override func viewDidLoad() {
         super.viewDidLoad()
+        textFieldOutlet.delegate = self
+        ageOutlet.delegate = self
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func buttonPress(_ sender: UIButton) {
+        theName = textFieldOutlet.text
+        theAge = ageOutlet.text
+        labelOutlet.text = theName + " is " + theAge + " years old!"
+        textFieldOutlet.resignFirstResponder()
+        ageOutlet.resignFirstResponder()
+    }
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textFieldOutlet.resignFirstResponder()
+        ageOutlet.resignFirstResponder()
+        return true
+    }
 }
 
